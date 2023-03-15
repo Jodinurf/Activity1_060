6a4ebf6d403dbe302827e04ee4989e38eaba4991
Jodinurf <jodinurfarkhani04@gmail.com>
2023-03-15 11:42:41 +0700

    Membuat program dengan if else

---
+++ kondisi2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b){
+    return (a+b)/2;
+}
+
+string status (double c){
+    if (c >= 60)
+    return "Lulus";
+    else
+    return "Gagal";
+}
+
+string status2 (double r, double n){
+    if (r >= 60 && n >= 70)
+    return "Lulus";
+    else
+    return "Gagal";
+}
+
+string status3 (double r, double n, double m){
+    if (r >= 60 || n >= 70 && m >= 60)
+    return "Lulus";
+    else
+    return "gagal";
+}
+
+int main(){
+    double nilM,nilB;
+
+    cout << "Masukkan Nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan Nilai Bahasa = ";
+    cin >> nilB;
+    
+
+cout<< "Status Kelulusan = " << status(rerata(nilM,nilB));
+cout<< "\nStatus Kelulusan ke 2 = " << status2(rerata(nilM,nilB),nilM);
+cout<< "\nStatus Kelulusan ke 3 = " << status3(rerata(nilM,nilB),nilM,nilB);
+return 0;
+}
